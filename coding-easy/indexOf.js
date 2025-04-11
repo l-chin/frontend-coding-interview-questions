249d1a40d13e23e6924edb7334a9c89b53ef36bb
l_qin <lqin3131@hotmail.com>
2025-04-11 17:14:30 +0800

    finished 9 questions

--- coding-easy/indexOf.js
+++ coding-easy/indexOf.js
@@ -1,3 +1,11 @@
+// question: implement indexOf function
+// indexOf function takes an array and an item as arguments
+// it returns the index of the item in the array
+// if the item is not found, it returns -1
+// example:
+// indexOf([1, 2, 3], 1) // 0
+// indexOf([1, 2, 3], 4) // -1
+
 /// solution
 
 function indexOf(array, item) {
@@ -8,6 +16,34 @@ function indexOf(array, item) {
   }
   return -1
 }
+// time complexity: O(n)
+// space complexity: O(1)
+
+// practice:
+const indexOfTest1 = (array, item) => {
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] === item) return i
+  }
+  return -1
+}
+// time complexity: O(n)
+// space complexity: O(1)
+
+// solution 2: using double pointers
+const indexOfTest2 = (array, item) => {
+  let left = 0
+  let right = array.length - 1
+  while(left <= right) {
+    if(array[left] === item) return left  
+    if(array[right] === item) return right
+    left++
+    right--
+  }
+  return -1
+}
+// time complexity: O(n)
+// space complexity: O(1)
+// theoretically, this solution is faster than the first one, but in practice, the first one is faster
 
 /// tests
 
@@ -15,3 +51,9 @@ import { test } from 'ava'
 
 test(t => t.is(indexOf([1, 2, 3], 1), 0))
 test(t => t.is(indexOf([1, 2, 3], 4), -1))
+
+test(t => t.is(indexOfTest1([1, 2, 3], 1), 0))
+test(t => t.is(indexOfTest1([1, 2, 3], 4), -1))
+
+test(t => t.is(indexOfTest2([1, 2, 3], 1), 0))
+test(t => t.is(indexOfTest2([1, 2, 3], 4), -1))
--- coding-easy/isPalindrome.js
+++ coding-easy/isPalindrome.js
@@ -1,5 +1,12 @@
-/// solution
+// question: implement isPalindrome function
+// isPalindrome function takes a string as an argument
+// it returns true if the string is a palindrome
+// otherwise, it returns false
+// example:
+// isPalindrome('abcdcba') // true
+// isPalindrome('abcd') // false
 
+/// solution
 function isPalindrome(string) {
   let _string = string.replace(/\s/g, '').toLowerCase()
   return reverse(_string) === _string
@@ -8,6 +15,32 @@ function isPalindrome(string) {
 function reverse(string) {
   return string.split('').reverse().join('')
 }
+// time complexity: O(n)
+// space complexity: O(n)
+
+// practice:
+const isPalindromeTest1 = (str) => {
+  let sanitizedStr = str.replaceAll(" ", '').toLowerCase()
+  let reversedSanitizedStr = sanitizedStr.split('').reverse().join('')
+  return reversedSanitizedStr === sanitizedStr
+}
+// time complexity: O(n)
+// space complexity: O(n)
+
+// solution 2: using double pointers
+const isParlindromeTest2 = (str) => {
+  let sanitizedStr = str.replaceAll(" ", '').toLowerCase()
+  let left = 0
+  let right = sanitizedStr.length - 1
+  while (left < right) {
+    if(sanitizedStr[left] !== sanitizedStr[right]) return false
+    left++
+    right--
+  }
+  return true
+}
+// time complexity: O(n)
+// space complexity: O(1)
 
 /// tests
 
@@ -17,3 +50,14 @@ test(t => t.is(isPalindrome(''), true))
 test(t => t.is(isPalindrome('abcdcba'), true))
 test(t => t.is(isPalindrome('abcd'), false))
 test(t => t.is(isPalindrome('A man a plan a canal Panama'), true))
+
+test(t => t.is(isPalindromeTest1(''), true))
+test(t => t.is(isPalindromeTest1('abcdcba'), true))
+test(t => t.is(isPalindromeTest1('abcd'), false))
+test(t => t.is(isPalindromeTest1('A man a plan a canal Panama'), true))
+
+test(t => t.is(isParlindromeTest2(''), true))
+test(t => t.is(isParlindromeTest2('abcdcba'), true))
+test(t => t.is(isParlindromeTest2('abcd'), false))
+test(t => t.is(isParlindromeTest2('A man a plan a canal Panama'), true))
+
